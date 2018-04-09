8a5a29b0027f8368b8005c190214e78e257417c4
b2017-17viu16m <b2017-17viu16m@yandex.ru>
2018-04-09 21:35:10 +0300

    added print package

---
+++ cmake/projects/print/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    print
+    VERSION
+    "0.1.0.0"
+    URL
+    "https://github.com/b2017-17viu16m/lab09/archive/v0.1.0.0.tar.gz"
+    SHA1
+    62f1c28b38044a9fe54a1a4fe7e297754ff9124e
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    print
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(print)
+hunter_download(PACKAGE_NAME print)
